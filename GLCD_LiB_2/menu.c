cd1fe29f6084b9e4396c92757219ff5740815c37
root <root@embeddedfab-Satellite-A135>
2018-11-22 16:09:33 +0200

    test first version of menus

--- GLCD_LiB_2/menu.c
+++ GLCD_LiB_2/menu.c
@@ -1,38 +1,38 @@
 /*
 
-  main.c 
-  
+  main.c
+
   Graphics Test for ATMEGA Controller
 
   Universal 8bit Graphics Library
-  
+
   Copyright (c) 2012, olikraus@gmail.com
   All rights reserved.
 
-  Redistribution and use in source and binary forms, with or without modification, 
+  Redistribution and use in source and binary forms, with or without modification,
   are permitted provided that the following conditions are met:
 
-  * Redistributions of source code must retain the above copyright notice, this list 
+  * Redistributions of source code must retain the above copyright notice, this list
     of conditions and the following disclaimer.
-    
-  * Redistributions in binary form must reproduce the above copyright notice, this 
-    list of conditions and the following disclaimer in the documentation and/or other 
+
+  * Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
     materials provided with the distribution.
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
-  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 */
 
 #include "u8g.h"
@@ -44,6 +44,14 @@
 #include "util/delay.h"
 
 #include "MCAL/EF_UART.h"
+#include "menu.h"
+
+
+
+
+
+
+
 
 
 
@@ -93,28 +101,13 @@ static char memory_card_28_bits[] U8G_PROGMEM = {
 0xF8, 0xFF, 0xFF, 0x01, };
 
 
-/* 
-  Software SPI:
-  uint8_t u8g_InitSPI(u8g_t *u8g, u8g_dev_t *dev, uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset); 
 
-  Hardware SPI:
-  uint8_t u8g_InitHWSPI(u8g_t *u8g, u8g_dev_t *dev, uint8_t cs, uint8_t a0, uint8_t reset);
-
-  Parallel Interface:
-  uint8_t u8g_Init8Bit(u8g_t *u8g, u8g_dev_t *dev, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, 
-    uint8_t en, uint8_t cs1, uint8_t cs2, uint8_t di, uint8_t rw, uint8_t reset);
-
-  Visit 
-    http://code.google.com/p/u8glib/wiki/device 
-  for a list of valid devices (second argument of the constructor).
-
-  The following examples will use the dogm132 device: u8g_dev_st7565_dogm132_sw_spi
-
-  Note: The device must match the setup: For example, do not use a sw_spi device with u8g_InitHWSPI().
-*/
 
 u8g_t u8g;
 uint8_t draw_state = 0;
+
+uint8_t volatile homeDisplay_flag = 0;
+uint8_t volatile setupDisplay_flag = 0;
 
 
 
@@ -126,58 +119,18 @@ uint8_t sys_key_code = KEY_NONE;
 static UART_cfg_str uart_cfg1 = {9600, 8, ONE_STOP_BIT, NO_PARITY, FALSE, FALSE, TRUE, TRUE};
 
 
+
+
+
 void u8g_setup(void)
-{  
-  /*
-    Test Envionment 1, ATMEGA and DOGM132 
-    CS: PORTB, Bit 2
-    A0: PORTB, Bit 1
-    SCK: PORTB, Bit 5
-    MOSI: PORTB, Bit 3
-  */
-  // u8g_InitSPI(&u8g, &u8g_dev_st7565_dogm132_sw_spi, PN(1, 5), PN(1, 3), PN(1, 2), PN(1, 1), U8G_PIN_NONE);
-
-  /*
-    Test Envionment 2, ATMEGA and Seeedstudio 96x96 OLED via I2C
-    SCL: Port C, Bit 5
-    SDA: Port C, Bit 4
-  */
-  
-  /* activate pull-up, set ports to output, init U8glib */  
-  /*
-  u8g_SetPinInput(PN(2,5)); u8g_SetPinLevel(PN(2,5), 1); u8g_SetPinOutput(PN(2,5));
-  u8g_SetPinInput(PN(2,4)); u8g_SetPinLevel(PN(2,4), 1); u8g_SetPinOutput(PN(2,4));
-  u8g_InitI2C(&u8g, &u8g_dev_ssd1327_96x96_gr_i2c, U8G_I2C_OPT_NONE);
-  */
-
-  /*
-    Test Envionment 3, ATMEGA and NHD 192x32 ST7920 special SPI
-    R/W, MOSI, Red: 	Port C, Bit 5
-    RS, CS, Yellow: 	Port C, Bit 4
-    EN, SCK, Green:	Port C, Bit 3
-    Arguments for u8g_InitSPI are: SCK, MOSI, CS, A0, Reset
-      A0 and Reset are not used.
-  */
-  //u8g_InitSPI(&u8g, &u8g_dev_st7920_192x32_sw_spi, PN(2, 3), PN(2, 5), PN(2, 4), U8G_PIN_NONE, U8G_PIN_NONE);
-
-  /*
-    Test Envionment 4, ATMEGA and NHD 192x32 ST7920 special SPI
-    R/W, MOSI, Red: 	Port B, Bit 3
-    RS, CS, Yellow: 	Port C, Bit 4
-    EN, SCK, Green:	Port B, Bit 5
-    Arguments for u8g_InitHWSPI are: CS, A0, Reset
-      A0 and Reset are not used.
-  */
-//  u8g_InitHWSPI(&u8g, &u8g_dev_st7920_192x32_hw_spi, PN(2, 4), U8G_PIN_NONE, U8G_PIN_NONE);
-
-
+{
 	  u8g_InitSPI(&u8g, &u8g_dev_st7920_128x64_hw_spi, PN(1, 7), PN(1, 5), PN(1, 4), U8G_PIN_NONE, U8G_PIN_NONE);
 	  u8g_InitHWSPI(&u8g, &u8g_dev_st7920_128x64_hw_spi, PN(1, 4), U8G_PIN_NONE, U8G_PIN_NONE);
-
-
-
-
 	  EF_void_UART_Init(&uart_cfg1);
+
+
+
+
 
   /* flip screen, if required */
 //  u8g_SetRot180(&u8g);
@@ -335,7 +288,15 @@ void sys_debounce_key(void)
 
 
 #define MENU_ITEMS 4
-char *menu_strings[MENU_ITEMS] = { "1) Enter Machine Number", "2) Enter User Name ", "3) Enter Your Password", "4) Home Menu" };
+char *menu_strings[MENU_ITEMS] = { "1) Enter Machine Number", "2) Enter User Name ", "3) Enter Your Password", "4) Check Authentication" };
+
+#define HOME_MENU_ITEMS 4
+char *home_menu_strings[MENU_ITEMS] = { "1) My Services ", "2) Software Update ", "3) About the device", "4) Shut Down" };
+
+#define COINS_MENU_ITEMS 4
+char *coins_menu_strings[MENU_ITEMS] = { "1) Enter Customer Pin", "2) Enter Customer Code ", "3) Check Customer Discount  ", "4) Thank you" };
+
+
 
 volatile uint8_t menu_current = 0;
 volatile uint8_t menu_redraw_required = 0;
@@ -349,11 +310,7 @@ void draw_menu(void)
 //  u8g_SetFont(&u8g, u8g_font_5x7);
   u8g_SetFontRefHeightText(&u8g);
   u8g_SetFontPosTop(&u8g);
-
-
   u8g_DrawStr(&u8g, 15, 3,"Setup Menu");
-
-
   h = u8g_GetFontAscent(&u8g)-u8g_GetFontDescent(&u8g);
   w = u8g_GetWidth(&u8g);
   for( i = 0; i < MENU_ITEMS; i++ )
@@ -376,41 +333,82 @@ void draw_menu(void)
 
 
 
-void draw(void) {
+
+void draw_home_menu(void)
+{
+  uint8_t i, h;
+  u8g_uint_t w, d;
+
+//  u8g_SetFont(&u8g, u8g_font_5x7);
+
+//	u8g_prepare();
+
+  u8g_SetFontRefHeightText(&u8g);
+  u8g_SetFontPosTop(&u8g);
 
 
-//	EF_void_UART_SendString("draw state ");
-//	EF_void_UART_Send_Integer(draw_state >> 3);
-//	EF_void_UART_SendString("\n");
+  u8g_DrawStr(&u8g, 15, 3,"Home Menu");
 
 
-//  switch(draw_state >> 3)
-  switch(draw_state)
+  h = u8g_GetFontAscent(&u8g)-u8g_GetFontDescent(&u8g);
+  w = u8g_GetWidth(&u8g);
+  for( i = 0; i < HOME_MENU_ITEMS; i++ )
   {
+//    d = (w-u8g_GetStrWidth(&u8g, menu_strings[i]))/2;
+    u8g_SetDefaultForegroundColor(&u8g);
+    if ( i == menu_current )
+    {
+      u8g_DrawBox(&u8g, 0, (i+2)*h+1, w, h);
+      u8g_SetDefaultBackgroundColor(&u8g);
 
+    }
+//    u8g_DrawStr(&u8g, d, i*h, menu_strings[i]);
+    u8g_DrawStr(&u8g, 0, ((i+2)*h), home_menu_strings[i]);
 
-//  case 0: draw_menu();break;
-
-
-      case 0: U8g_welcom(); break;
-  //    case 1: u8g_box_frame(draw_state&7); break;
-  //    case 2: u8g_string(draw_state&7); break;
-  //    case 3: u8g_line(draw_state&7); break;
-      case 1: draw_menu();break;
-  //    case 3: u8g_ascii_1(); break;
-  //    case 4: u8g_ascii_2(); break;
   }
+
+}
+
+
+void draw_coin_menu(void)
+{
+  uint8_t i, h;
+  u8g_uint_t w, d;
+
+  u8g_SetFontRefHeightText(&u8g);
+  u8g_SetFontPosTop(&u8g);
+  u8g_DrawStr(&u8g, 15, 3,"Share Coins Menu");
+  h = u8g_GetFontAscent(&u8g)-u8g_GetFontDescent(&u8g);
+  w = u8g_GetWidth(&u8g);
+  for( i = 0; i < COINS_MENU_ITEMS; i++ )
+  {
+    u8g_SetDefaultForegroundColor(&u8g);
+    if ( i == menu_current )
+    {
+      u8g_DrawBox(&u8g, 0, (i+2)*h+1, w, h);
+      u8g_SetDefaultBackgroundColor(&u8g);
+
+    }
+    u8g_DrawStr(&u8g, 0, ((i+2)*h), coins_menu_strings[i]);
+
+  }
+
 }
 
 
 
 
-void update_menu(void)
+
+
+
+
+char update_menu(void)
 {
+	char key_selected = -1;
 
 	if ( sys_key_code != KEY_NONE && last_key_code == sys_key_code )
 	{
-		return;
+		return -1;
 	}
 	last_key_code = sys_key_code;
 
@@ -430,68 +428,74 @@ void update_menu(void)
 		break;
 
 	case KEY_SELECT:
+		key_selected =  menu_current;
 
-		switch(menu_current)
-		{
-		case 0:
-			u8g_FirstPage(&u8g);
-			do{
-				u8g_DrawStr(&u8g,30,5, "Memory Card");
-				u8g_DrawXBMP(&u8g,50,25,28,28,memory_card_28_bits);
+		break;
 
-
-			}while ( u8g_NextPage(&u8g) );
-			break;
-
-		case 1:
-
-			u8g_FirstPage(&u8g);
-			do{
-				  u8g_DrawStr(&u8g,45,5,"Tool");
-				  u8g_DrawXBMP(&u8g,45,25,28,28,tools_28_bits);
-
-
-			}while ( u8g_NextPage(&u8g) );
-
-			break;
-
-		case 3:
-
-			EF_void_UART_SendString("select the fourth item\n");
-
-			u8g_prepare();
-
-
-
-			u8g_FirstPage(&u8g);
-			do{
-
-				  u8g_DrawStr(&u8g,35,15,"Thank You");
-				  u8g_DrawStr(&u8g,55,30,":)");
-
-
-//				u8g_DrawXBMP(&u8g,10,15,28,28,clock_28_bits);
-
-			}while ( u8g_NextPage(&u8g) );
-//			_delay_ms(50);
-			break;
-
-
-		case 2:
-			u8g_FirstPage(&u8g);
-			do{
-				 u8g_DrawStr(&u8g,40,5,"Clock");
-				 u8g_DrawXBMP(&u8g,40,25,28,28,clock_28_bits);
-			}while ( u8g_NextPage(&u8g) );
-			break;
-
-
-			//    	default:
-			//    		break;
-		}/* end inner SW  */
-
-//		break;
 	}//outer SW
+
+	return key_selected;
+
+
+
+//		switch(menu_current)
+//		{
+//		case 0:
+//			u8g_FirstPage(&u8g);
+//			do{
+//				u8g_DrawStr(&u8g,30,5, "Memory Card");
+//				u8g_DrawXBMP(&u8g,50,25,28,28,memory_card_28_bits);
+//
+//
+//			}while ( u8g_NextPage(&u8g) );
+//			break;
+//
+//		case 1:
+//
+//			u8g_FirstPage(&u8g);
+//			do{
+//				  u8g_DrawStr(&u8g,45,5,"Tool");
+//				  u8g_DrawXBMP(&u8g,45,25,28,28,tools_28_bits);
+//			}while ( u8g_NextPage(&u8g) );
+//
+//			break;
+//
+//		case 3:
+//
+//
+////			 sys_setup_keys();
+////			  u8g_SetFont(&u8g, u8g_font_5x8r);
+//			  u8g_SetFontRefHeightExtendedText(&u8g);
+//			  u8g_SetDefaultForegroundColor(&u8g);
+////			  u8g_SetFontPosTop(&u8g);
+//			u8g_FirstPage(&u8g);
+//			do{
+//				  u8g_DrawStr(&u8g,35,15,"Please Wait...");
+//				  u8g_DrawStr(&u8g,15,25,"check authentication ");
+//				  /* check the authentication here */
+//				  _delay_ms(100);
+//				  u8g_DrawStr(&u8g,35,45,"User Verified");
+//				  homeDisplay_flag = 1;
+//
+//
+//			}while ( u8g_NextPage(&u8g) );
+//			break;
+//
+//
+//		case 2:
+//			u8g_FirstPage(&u8g);
+//			do{
+//				 u8g_DrawStr(&u8g,40,5,"Clock");
+//				 u8g_DrawXBMP(&u8g,40,25,28,28,clock_28_bits);
+//			}while ( u8g_NextPage(&u8g) );
+
+
+//
+//			break;
+//
+//		}/* end inner SW  */
+
+
 }
 
 
@@ -499,46 +503,203 @@ void update_menu(void)
 
 int main(void)
 {
-	sys_init();
+
+	enu_Dsply_stat Dislay_stat = welcom_menu_stat;
+	//	enu_setup_menu setup_menu;
+	//	enu_home_menu home
+	char item_selected = -1;
+
+
 	u8g_setup();
 	menu_redraw_required = 1;
 	u8g_prepare();
-
-	 u8g_FirstPage(&u8g);
-	 do{
-		 U8g_welcom();
-	 }while ( u8g_NextPage(&u8g) );
-	  _delay_ms(1000);
-
 
 	for(;;)
 	{
 		sys_debounce_key();
 
-		if(menu_redraw_required != 0 )
+		switch(Dislay_stat)
 		{
+		case welcom_menu_stat:
 			u8g_FirstPage(&u8g);
-			do
+			do{
+				U8g_welcom();
+			}while ( u8g_NextPage(&u8g) );
+			Dislay_stat = setup_menu_stat;
+			_delay_ms(3000);
+			break;
+		case setup_menu_stat:
+
+			if (menu_redraw_required == 1)
 			{
-				draw_menu();
+				u8g_FirstPage(&u8g);
+				do
+				{
+					draw_menu();
+				} while ( u8g_NextPage(&u8g) );
+			}
+			/* setup menus action  */
 
-			} while ( u8g_NextPage(&u8g) );
+			switch(item_selected)
+			{
+			case setup_machine_num:
+				u8g_FirstPage(&u8g);
+				do{
+					u8g_DrawStr(&u8g,30,5, "Memory Card");
+					u8g_DrawXBMP(&u8g,50,25,28,28,memory_card_28_bits);
+				}while ( u8g_NextPage(&u8g) );
+				break;
+			case setup_user_name:
 
-			menu_redraw_required = 0;
-		}
+				break;
+			case setup_password:
+				u8g_FirstPage(&u8g);
+				do{
+					u8g_DrawStr(&u8g,40,5,"Clock");
+					u8g_DrawXBMP(&u8g,40,25,28,28,clock_28_bits);
+				}while ( u8g_NextPage(&u8g) );
 
-		update_menu();
+				break;
+			case setup_athntcation:
+				u8g_SetFontRefHeightExtendedText(&u8g);
+				u8g_SetDefaultForegroundColor(&u8g);
+				//			  u8g_SetFontPosTop(&u8g);
+				u8g_FirstPage(&u8g);
+				do{
+					u8g_DrawStr(&u8g,35,15,"Please Wait...");
+					u8g_DrawStr(&u8g,15,25,"check authentication ");
+					/* check the authentication here */
+					_delay_ms(100);
+					u8g_DrawStr(&u8g,35,45,"User Verified");
+					homeDisplay_flag = 1;
 
-//		EF_void_UART_SendString("update menu redraw ");
-//		EF_void_UART_Send_Integer(menu_redraw_required);
-//		EF_void_UART_SendString("\n");
+
+				}while ( u8g_NextPage(&u8g) );
+
+				break;
 
 
 
-//		draw_state++;
-//		if ( draw_state >= 2 )
-//			draw_state = 0;
+			}/*end SW case*/
 
-		u8g_Delay(100);
-	}
+
+
+
+			if(homeDisplay_flag == 1)
+			{
+				Dislay_stat = home_menu_stat;
+				menu_redraw_required = 1;
+				menu_current = 0;
+
+				EF_void_UART_SendString("move to home menu\n");
+			}
+			else{
+				menu_redraw_required = 0;
+			}
+
+
+			break;
+
+			case home_menu_stat:
+
+
+				//EF_void_UART_SendString("home menu state\n");
+
+				/*check home display flag */
+				if(menu_redraw_required == 1)
+				{
+					EF_void_UART_SendString("draw  home menu\n");
+					u8g_prepare();
+					u8g_FirstPage(&u8g);
+					do
+					{
+						draw_home_menu();
+					} while ( u8g_NextPage(&u8g) );
+					menu_redraw_required = 0;
+
+				}
+
+				switch(item_selected)
+				{
+				case home_service:
+
+					//					Dislay_stat = sharCoin_menu_stat;
+					if(homeDisplay_flag == 1)
+					{
+						Dislay_stat = sharCoin_menu_stat;
+						menu_redraw_required = 1;
+						menu_current = 0;
+
+						EF_void_UART_SendString("move to share coins menu\n");
+					}
+					else{
+						menu_redraw_required = 0;
+					}
+
+
+
+					break;
+				case home_SwUpdate:
+					break;
+				case home_device:
+					break;
+				case home_shutDown:
+					break;
+				}
+
+				break;
+
+			case sharCoin_menu_stat:
+				if (menu_redraw_required == 1)
+				{
+					u8g_prepare();
+
+					u8g_FirstPage(&u8g);
+					do
+					{
+						draw_coin_menu();
+					} while ( u8g_NextPage(&u8g) );
+					menu_redraw_required = 0;
+				}
+
+				switch(item_selected)
+				{
+				case share_customer_discont:
+
+					EF_void_UART_SendString("draw the discount points\n");
+
+					u8g_FirstPage(&u8g);
+
+					do{
+					u8g_DrawStr(&u8g,35,5,"Please Wait...");
+					u8g_DrawStr(&u8g,30,15,"check discount..");
+					/* check the authentication here */
+					_delay_ms(200);
+					u8g_DrawStr(&u8g,30,25,"Congrats you win");
+					u8g_DrawStr(&u8g,40,35,"120 coins");
+
+//					homeDisplay_flag = 1;
+
+
+					}while ( u8g_NextPage(&u8g) );
+
+				}
+
+				break;  /* */
+				}
+
+
+//				break;
+
+
+				item_selected = update_menu();
+
+
+				u8g_Delay(50);
+
+		}/*end Super loop*/
+
+
+
+
 }
